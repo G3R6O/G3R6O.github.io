439e355b39d06fe1276ce97071c3b8d26d322b27
G3R6O <97744951+G3R6O@users.noreply.github.com>
2022-09-04 21:25:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -29,64 +29,91 @@ let databinding = d3.json("data.json").then(function (data) {
     .call(d3.axisBottom(x))
     .selectAll("text")
     .attr("transform", "translate(-10,0)rotate(-45)")
+
     .style("text-anchor", "end");
 
   // Add Y axis
   const y = d3.scaleLinear()
-    .domain([0, d3.max(data, function (d) { return d.SX+d.Y3; })])
+    .domain([0, d3.max(data, function (d) { return d.SX + d.Y3; })])
     //.range([ height, 0]);
     .range([height, 0]);
-   var yAxis = d3.axisLeft()
+  var yAxis = d3.axisLeft()
     .scale(y)
     .tickFormat(function (d) {
       if ((d / 1000) >= 1) {
         d = d / 1000 + "K";
       }
       return d;
-    });
+    })
+    ;
 
   svg.append("g")
-    .call(yAxis);
+    .call(yAxis)
+    .attr("class", "axisBlue")
+    ;
 
   // Add Y2 axis
   const y2 = d3.scaleLinear()
-  .domain([0, d3.max(data, function (d) { return d.price; })])
-  //.range([ height, 0]);
-  .range([height, 0]);
- var y2Axis = d3.axisRight()
-  .scale(y2);
+    .domain([0, d3.max(data, function (d) { return d.price; })])
+    //.range([ height, 0]);
+    .range([height, 0]);
+  var y2Axis = d3.axisRight()
+    .scale(y2);
 
-svg.append("g")
-  .attr("transform", `translate(${width},0)`)
-  .call(y2Axis);
-
+  svg.append("g")
+    .attr("transform", `translate(${width},0)`)
+    .call(y2Axis)
+    .attr("class", "axisGrey")
+    ;
+    svg.append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("y", 6)
+    .attr("dy", ".75em")
+    .attr("transform", "rotate(-90)")
+    .text("# cars produced")
+    .attr("fill", "steelblue");
+    svg.append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("y", width-20)
+    .attr("dy", ".75em")
+    .attr("transform", "rotate(-90)")
+    .text("stock price")
+    .attr("fill", "grey");
+    svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "middle")
+    .attr("x", width/2)
+    .attr("y", height +60)
+    .text("quarters");
   // Bars
   svg.selectAll('.mybar')
     .data(data)
     .join("rect")
     .attr("x", d => x(d.Q))
-    .attr("width", x.bandwidth()/2)
+    .attr("width", x.bandwidth() / 2)
     .attr("fill", "steelblue")
-    .classed('mybar',true)
-    .on("mouseover", function (event,d) {
+    .classed('mybar', true)
+    .on("mouseover", function (event, d) {
       d3.select(this)
-      .transition().duration(200)
-      .attr("fill", "#ff6f3c");
+        .transition().duration(200)
+        .attr("fill", "#ff6f3c");
       div.transition()
-      .duration(200)
-      .style("opacity", .9);
-    div.html("<b> " + d.Q + "</b> <br/>" + "S/X: " + 
-    d.SX.toLocaleString('en')+"<br/> 3/Y: "+ d.Y3.toLocaleString('en')
-    +"<br/><b>  "+ (d.Y3+d.SX).toLocaleString('en')+"</b>")
-      .style("left", (event.pageX) - 35 + "px")
-      .style("top", (event.pageY - 100) + "px");
+        .duration(200)
+        .style("opacity", .9);
+      div.html("<b> " + d.Q + "</b> <br/>" + "S/X: " +
+        d.SX.toLocaleString('en') + "<br/> 3/Y: " + d.Y3.toLocaleString('en')
+        + "<br/><b>  " + (d.Y3 + d.SX).toLocaleString('en') + "</b>")
+        .style("left", (event.pageX) - 35 + "px")
+        .style("top", (event.pageY - 100) + "px");
     })
     .on("mouseout", function (d) {
       d3.select(this).transition()
-      .duration(500).attr("fill", "steelblue");
+        .duration(500).attr("fill", "steelblue");
       div.transition()
-         .duration(500)
-         .style("opacity", 0);
+        .duration(500)
+        .style("opacity", 0);
     })
     // no bar at the beginning thus:
     .attr("height", d => height - y(0)) // always equal to 0
@@ -96,37 +123,37 @@ svg.append("g")
   svg.selectAll('.mybar')
     .transition()
     .duration(250)
-    .delay(function(d, i) { return i * 100; })
-    .attr("y", d => y(d.SX+d.Y3))
-    .attr("height", d => height - y(d.SX+d.Y3))
+    .delay(function (d, i) { return i * 100; })
+    .attr("y", d => y(d.SX + d.Y3))
+    .attr("height", d => height - y(d.SX + d.Y3))
 
-    //price
+  //price
 
-    svg.selectAll('.mybar2')
+  svg.selectAll('.mybar2')
     .data(data)
     .join("rect")
-    .attr("x", d => x(d.Q)+x.bandwidth()/2)
-    .attr("width", x.bandwidth()/2)
+    .attr("x", d => x(d.Q) + x.bandwidth() / 2)
+    .attr("width", x.bandwidth() / 2)
     .attr("fill", "grey")
-    .classed('mybar2',true)
-    .on("mouseover", function (event,d) {
+    .classed('mybar2', true)
+    .on("mouseover", function (event, d) {
       d3.select(this)
-      .transition().duration(200)
-      .attr("fill", "#ff6f3c");
+        .transition().duration(200)
+        .attr("fill", "#ff6f3c");
       div.transition()
-      .duration(200)
-      .style("opacity", .9);
-    div.html("<b> " + d.Q + "</b> <br/>" + "Avg. price: <br/><b>" + 
-    d.price.toLocaleString('en')+"</b>")
-      .style("left", (event.pageX) - 35 + "px")
-      .style("top", (event.pageY - 100) + "px");
+        .duration(200)
+        .style("opacity", .9);
+      div.html("<b> " + d.Q + "</b> <br/>" + "Avg. price: <br/><b>" +
+        d.price.toLocaleString('en') + "</b>")
+        .style("left", (event.pageX) - 35 + "px")
+        .style("top", (event.pageY - 100) + "px");
     })
     .on("mouseout", function (d) {
       d3.select(this).transition()
-      .duration(500).attr("fill", "grey");
+        .duration(500).attr("fill", "grey");
       div.transition()
-         .duration(500)
-         .style("opacity", 0);
+        .duration(500)
+        .style("opacity", 0);
     })
     // no bar at the beginning thus:
     .attr("height", d => height - y(0)) // always equal to 0
@@ -136,10 +163,8 @@ svg.append("g")
   svg.selectAll('.mybar2')
     .transition()
     .duration(250)
-    .delay(function(d, i) { return i * 100; })
+    .delay(function (d, i) { return i * 100; })
     .attr("y", d => y2(d.price))
     .attr("height", d => height - y2(d.price))
 
 })
-
-
